d2d5c670c6c85437af6fe4b58a8ef373c99eb672
François <frdebellescize@hotmail.fr>
2019-12-18 14:53:18 +0100

    objective 2

--- script.js
+++ script.js
@@ -7,35 +7,59 @@ const moves = {
   'rock': 'paper',
 }
 
+const computerPlays = {
+  'scissors': 0,
+  'paper': 0,
+  'rock': 0,
+}
+
 init();
 
 async function init() {
   const preferred = await startGame();
-  play(preferred);
+  play();
 }
 
 async function startGame() {
   const response = await fetch('http://the-rock-paper-scissors.herokuapp.com/newgame');
-  const text = await response.text();
-  return text;
 }
 
-async function play(preferred) {
+function pickBestCounter() {
+  if (computerPlays.scissors >= computerPlays.paper && computerPlays.scissors >= computerPlays.rock) {
+    return moves.scissors;
+  }
+  else if (computerPlays.paper >= computerPlays.scissors && computerPlays.paper >= computerPlays.rock) {
+    return moves.paper;
+  }
+  else if (computerPlays.rock >= computerPlays.paper && computerPlays.rock >= computerPlays.scissors) {
+    return moves.rock;
+  }
+}
+
+async function play() {
   computerScore = 0;
   score = 0;
 
-  const counter = moves[preferred];
   console.log('You : Computer')
   while(score < 20 && computerScore < 20) {
+    const counter = pickBestCounter();
     const response = await fetch(`http://the-rock-paper-scissors.herokuapp.com?move=${counter}`);
     const text = await response.text();
 
     if (text === '1') {
       score += 1;
+      computerPlays[moves[counter]] += 1;
+    } else if (text === '0') {
+      computerPlays[counter] += 1;
     } else if (text === '-1') {
       computerScore += 1;
+      computerPlays[moves[counter]] += 1;
     }
     console.log(`${score} : ${computerScore}`);
   }
-  console.log(score);
+  if (score > computerScore) {
+    console.log("You won !");
+  } else {
+    console.log("You loose !");
+  }
 }
